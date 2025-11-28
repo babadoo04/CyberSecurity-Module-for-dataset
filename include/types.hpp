7ba05c7620fec7d234641d8bfc51f2495f8eb0a2
Yanenko Mykyta <yanenkomykyta@gmail.com>
2025-11-28 14:01:32 +0200

    refactor file types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -3,23 +3,20 @@
 
 #include "pch.hpp"
 
-namespace cryptTools {
+struct encryptedFile {
+    std::vector<std::uint8_t> salt;
+    std::vector<std::uint8_t> iv;
+    std::vector<std::uint8_t> tag;
+    std::vector<std::uint8_t> ciphertext;
 
-    struct encryptedFile {
-        std::vector<std::uint8_t> salt;
-        std::vector<uint8_t> iv;
-        std::vector<uint8_t> tag;
-        std::vector<uint8_t> ciphertext;
+    encryptedFile();
+    ~encryptedFile() = default;
 
-        encryptedFile();
-        ~encryptedFile() = default;
+    encryptedFile(encryptedFile&& other) noexcept;
+    encryptedFile& operator=(encryptedFile&& other) noexcept;
 
-        encryptedFile(encryptedFile&& other) noexcept;
-        encryptedFile& operator=(encryptedFile&& other) noexcept;
-
-        encryptedFile(const encryptedFile&) = delete;
-        encryptedFile& operator=(const encryptedFile&) = delete; 
-    };
-}
+    encryptedFile(const encryptedFile&) = delete;
+    encryptedFile& operator=(const encryptedFile&) = delete; 
+};
 
 #endif // !TYPES_HPP
