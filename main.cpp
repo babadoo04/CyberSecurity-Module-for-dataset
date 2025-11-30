b26ed437e7d7229c1c40ef72df6cd487f63eea93
Yanenko Mykyta <yanenkomykyta@gmail.com>
2025-11-30 13:49:32 +0200

    use env variables for password

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <cstdlib>
 
 #include "osRandomProvider.hpp"
 #include "pbkdf2.hpp"
 #include "aes256GcmCipher.hpp"
 #include "securityModule.hpp"
+
+std::string getEnvPassword() {
+    const char* p = std::getenv("DATASET_PASSPHRASE");
+    if (!p) throw std::runtime_error("DATASET_PASSPHRASE not set");
+    return std::string(p);
+}
 
 int main() {
     securityModule service(
@@ -12,25 +19,25 @@ int main() {
         std::make_unique<aes256GcmCipher>()
     );
 
-    const std::string passwd = "strong-passphrase1";
+    const std::string passwd = getEnvPassword();
 
     // 1) Encrypt
-    try {
-        service.encryptFile("netflix_titles.csv", "netflix_titles.enc", passwd);
-    } catch (const std::exception& e) {
-        std::cerr << "Encryption error: " << e.what() << "\n";
-        return -1;
-    }
-
+    // try {
+    //     service.encryptFile("example/Dataset_2.zip", "example/Dataset_2.enc", passwd);
+    // } catch (const std::exception& e) {
+    //     std::cerr << "Encryption error: " << e.what() << "\n";
+    //     return -1;
+    // }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
     // 2) Integrity check
-    if (!service.verifyIntegrity("netflix_titles.enc", passwd)) {
+    if (!service.verifyIntegrity("example/Dataset_2.enc", passwd)) {
         std::cerr << "Integrity FAIL: wrong password or corrupted file.\n";
         return -1;
     }
 
     // 3) Decrypt
     try {
-        service.decryptFile("netflix_titles.enc", "netflix_titles_dec.csv", passwd);
+        service.decryptFile("example/Dataset_2.enc", "example/Dataset_2_dec.zip", passwd);
     } catch (const std::exception& e) {
         std::cerr << "Decryption error: " << e.what() << "\n";
         return -1;
